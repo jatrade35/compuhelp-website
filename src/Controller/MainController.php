92d2e57505b955e2c651ad5841db1d4451ec82c9
Rene Lanteigne <jatrade35@hotmail.com>
2024-02-19 11:48:50 +0000

    Folder clean up and logo correction

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,18 +12,18 @@ class MainController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        $website = "";
-
         switch ($_SERVER['SERVER_NAME']) {
             case 'compuhelp-webdesign.ca':
                 $website = "Compuhelp Enterprises'";
                 $path = "compuhelp/";
+                $icon = "images/compuhelp/favicon.ico";
                 break;
             case 'health2wealthcoaching.compuhelp-webdesign.ca':
                 $website = "Heath2Wealth's";
                 $path = "health2wealth/";
+                $icon = "images/health2wealth/favicon.ico";
                 break;
         }            
-        return $this->render($path . 'welcome.html.twig', ['website'=>$website]);
+        return $this->render($path . 'welcome.html.twig', ['website'=>$website, 'icon'=>$icon]);
     }
 }
