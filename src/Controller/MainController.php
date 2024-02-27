368fd820930ee7b906db7649c4d62baa570cecd2
Rene Lanteigne <jatrade35@hotmail.com>
2024-02-27 07:18:13 +0000

    All basic pages drafted. Some work still left on home page, blog pages, form handling and About me page.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,6 +12,7 @@ class MainController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $page = 'welcome.html.twig';
         switch ($_SERVER['SERVER_NAME']) {
             case 'compuhelp-webdesign.ca':
                 $website = "Compuhelp Enterprises'";
@@ -23,7 +24,47 @@ class MainController extends AbstractController
                 $path = "health2wealth/";
                 $icon = "images/health2wealth/favicon.ico";
                 break;
+            case 'formatio.compuhelp-webdesign.ca':
+                $website = "fromatio's";
+                $path = "formatio/";
+                $icon = "images/formatio/favicon.ico";
+                break;
+            default:
+                $website = "";
+                $page = '404.html.twig';
+                $path = "compuhelp/";
+                $icon = "images/compuhelp/favicon.ico";
         }            
-        return $this->render($path . 'welcome.html.twig', ['website'=>$website, 'icon'=>$icon]);
+        return $this->render($path . $page, ['attributes'=>"class=\"rd-navbar-fixed-linked, lang=\"en\""]);
+    }
+
+    #[Route('/about-me', name: 'about me')]
+    public function about(): Response
+    {
+        $page = 'about-me.html.twig';
+        switch ($_SERVER['SERVER_NAME']) {
+            case 'compuhelp-webdesign.ca':
+                $path = "compuhelp/";
+                break;
+            default:
+                $path = "compuhelp/";
+                $page = '404.html.twig';
+        }            
+        return $this->render($path . $page, ['attributes'=>"class=\"rd-navbar-fixed-linked, lang=\"en\""]);
+    }
+
+    #[Route('/blog', name: 'blog')]
+    public function blog(): Response
+    {
+        $page = 'blog.html.twig';
+        switch ($_SERVER['SERVER_NAME']) {
+            case 'compuhelp-webdesign.ca':
+                $path = "compuhelp/";
+                break;
+            default:
+                $path = "compuhelp/";
+                $page = '404.html.twig';
+        }            
+        return $this->render($path . $page, ['attributes'=>"class=\"rd-navbar-fixed-linked, lang=\"en\""]);
     }
 }
